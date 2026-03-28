bbef473ee11ea7f50560224d156eac37b5d884dc
rrouwelng <rouwelngacha@gmail.com>
2026-03-28 17:21:00 +0300

    Tweepy works fine with the authentication but actual getting of data from  the api requires paying for the usage of a developer account

--- twitterwebsearch/searcher.py
+++ twitterwebsearch/searcher.py
@@ -3,13 +3,12 @@ Module for using the web interface of Twitter's search.
 """
 
 import tweepy
-import requests
 
-consumer_key = 'dwRRlaVZj7FYgyhnDM4UlY8QT'
-consumer_secret = 'IzoPhQvaGpn5d05OsHVFvyoiSd4b0PZ66bbN0mlzBnjTzrzySm'
-access_token = '2036531799300644864-bPqj6wL4G1vgUCpqiBDX3CPTkCojuU'
-access_token_secret = 'hmfPuAupb6AYq3L70uEti7DyWqKcKKjlrRiZygE9uNhyH'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAOUX8gEAAAAAAMmhZDykj3k4o4vC0Nh7FIXWCoE%3DedqCn7hOBAOcFTGJprY4mbg5hx0uZuWFHEPDv7UZKYS9NnDpv7'
+consumer_key = 'dw7FYgyhnDM4UlY8QT'
+consumer_secret = 'IzoPhiSd4b0Z66bbN0mlzBnjTzrzySm'
+access_token = '2036531793CPTkCojuU'
+access_token_secret = 'hmfPu'
+bearer_token = 'AAAAAAAAAAAAAAAAA'
 
 auth = tweepy.OAuth1UserHandler(consumer_key , consumer_secret, access_token, access_token_secret)
 api =  tweepy.API(auth)
@@ -21,6 +20,13 @@ except Exception as e:
 
     # Fetch the 5 most recent tweets from your timeline
 public_tweets = api.home_timeline(count=5)
-
 for tweet in public_tweets:
     print(f"{tweet.user.screen_name}: {tweet.text}\n")
+
+"""
+Output
+tweepy.errors.Forbidden: 403 Forbidden
+453 - You currently have access to a subset of X API V2 endpoints and limited v1.1 endpoints (e.g. media post, oauth) only. If you need access to this endpoint, you may need a different access level. You can learn more here: https://developer.x.com/en/portal/product
+
+
+"""
