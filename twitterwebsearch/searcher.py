dee3c56d8c29e16270fb619c5a1da19cfe89ee4a
rrouwelng <rouwelngacha@gmail.com>
2026-03-28 16:46:50 +0300

    Using a bot to access the api but is proving difficult, time to try the account

--- twitterwebsearch/searcher.py
+++ twitterwebsearch/searcher.py
@@ -3,6 +3,7 @@ Module for using the web interface of Twitter's search.
 """
 
 import tweepy
+import requests
 
 consumer_key = 'dwRRlaVZj7FYgyhnDM4UlY8QT'
 consumer_secret = 'IzoPhQvaGpn5d05OsHVFvyoiSd4b0PZ66bbN0mlzBnjTzrzySm'
@@ -10,11 +11,16 @@ access_token = '2036531799300644864-bPqj6wL4G1vgUCpqiBDX3CPTkCojuU'
 access_token_secret = 'hmfPuAupb6AYq3L70uEti7DyWqKcKKjlrRiZygE9uNhyH'
 bearer_token = 'AAAAAAAAAAAAAAAAAAAAAOUX8gEAAAAAAMmhZDykj3k4o4vC0Nh7FIXWCoE%3DedqCn7hOBAOcFTGJprY4mbg5hx0uZuWFHEPDv7UZKYS9NnDpv7'
 
-auth = tweepy.OAuth1UserHandler(
-    f"API /{consumer_key}" , f"API / {consumer_secret}", f"{access_token}", f"{access_token_secret}"
-)
-try:
-    api = tweepy.API(auth)
-    print("Achieved login")
-except:
-    print("login failed")
+auth = tweepy.OAuth1UserHandler(consumer_key , consumer_secret, access_token, access_token_secret)
+api =  tweepy.API(auth)
+
+try: 
+    api.verify_credentials()
+except Exception as e:
+    print(f"Error {e}")
+
+    # Fetch the 5 most recent tweets from your timeline
+public_tweets = api.home_timeline(count=5)
+
+for tweet in public_tweets:
+    print(f"{tweet.user.screen_name}: {tweet.text}\n")
